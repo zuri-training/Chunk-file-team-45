884f4bb1f60fca1da07d553157547e8ad419f531
oyewoledavid <64299405+oyewoledavid@users.noreply.github.com>
2022-07-27 17:11:12 +0100

    Views.py configured for user registration, login and logout

--- chucky/user/models.py
+++ chucky/user/models.py
@@ -1,4 +1,13 @@
+from email.policy import default
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+class Profile(models.Model):
+    
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
+    location=models.CharField(max_length=250, blank=True, null=True)
+    bio=models.TextField(blank=True, null=True)
+    avatar=models.ImageField(upload_to='profilepics/', blank=True, null=True, default='',)
 
+    def __str__(self):
+        return self.user
