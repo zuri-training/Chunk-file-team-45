8940633ed20044d5a9a17f114b8e2bb8b0c56fac
oyewoledavid <64299405+oyewoledavid@users.noreply.github.com>
2022-07-30 10:16:15 +0100

    url for user app done

--- chucky/user/urls.py
+++ chucky/user/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+from . import views
 
 
 app_name = 'user'
 
-urlpatterns = []
+urlpatterns = [
+    path('', views.home, name='homepage'),
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+]
