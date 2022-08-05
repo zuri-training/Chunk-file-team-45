19e1c290fe5da3a679a1b2a4ad98832dee482a13
oyewoledavid <64299405+oyewoledavid@users.noreply.github.com>
2022-08-05 15:35:01 +0100

    log in validation and some edit on the about page

--- frontend/login page/login.js
+++ frontend/login page/login.js
@@ -4,13 +4,68 @@ const menuRespond = document.querySelector(".responsive");
 let menuClosed = true;
 
 menuBtn.addEventListener("click", () => {
-  if(menuClosed) {
+  if (menuClosed) {
     menuBtn.classList.add("open");
     menuRespond.classList.add("responsiveBlock");
     menuClosed = false;
   } else {
     menuBtn.classList.remove("open");
-    menuClosed = true;   
+    menuClosed = true;
     menuRespond.classList.remove("responsiveBlock");
   }
-})
+});
+
+///VALIDATION////
+
+const form = document.getElementById("form");
+const userName = document.getElementById("username");
+const password = document.getElementById("password");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  checkInput();
+});
+
+function checkInput() {
+  const userNameValue = userName.value.trim();
+  const passwordValue = password.value.trim();
+
+  ///VALIDATE USERNAME//
+
+  if (userNameValue === "") {
+    setErrorFor(userName, "Username cannot be blank");
+  } else if (!validateUserName(userNameValue)) {
+    setErrorFor(userName, "only letters,numbers and underscore allowed");
+  } else {
+    setSuccessFor(userName);
+  }
+
+  if (passwordValue === "") {
+    setErrorFor(password, "Password cannot be blank");
+  } else if (passwordValue.length < 8 ) {
+    setErrorFor(password, "Password can not be less than 8 characters");
+  } else {
+    setSuccessFor(password);
+  }
+}
+
+function setErrorFor(input, message) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
+
+  small.innerText = message;
+
+  //Display the error class
+  formControl.className = "form-control error";
+}
+
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
+
+//USERNAME VALIDATOR
+function validateUserName(userName) {
+  const regex = /^[a-zA-Z0-9_]+$/;
+  return regex.test(userName);
+}
