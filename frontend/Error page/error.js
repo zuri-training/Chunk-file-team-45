d85be99a827d41572c46af3ff99178c000cadb10
oyewoledavid <64299405+oyewoledavid@users.noreply.github.com>
2022-08-07 23:15:18 +0100

    Error page

---
+++ frontend/Error page/error.js
@@ -0,0 +1,16 @@
+//toggle
+const menuBtn = document.querySelector(".menu__btn");
+const menuRespond = document.querySelector(".responsive");
+let menuClosed = true;
+
+menuBtn.addEventListener("click", () => {
+  if (menuClosed) {
+    menuBtn.classList.add("open");
+    menuRespond.classList.add("responsiveBlock");
+    menuClosed = false;
+  } else {
+    menuBtn.classList.remove("open");
+    menuClosed = true;
+    menuRespond.classList.remove("responsiveBlock");
+  }
+});
