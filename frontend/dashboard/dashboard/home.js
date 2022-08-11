eec35e09ae747b7c19fe4961c45691e76a01c9b6
oyewoledavid <64299405+oyewoledavid@users.noreply.github.com>
2022-08-11 11:44:12 +0100

    Worked on the radion buttons on the dashboard to disable input field of the oposite side when checked

--- frontend/dashboard/dashboard/home.js
+++ frontend/dashboard/dashboard/home.js
@@ -4,13 +4,31 @@ const menuRespond = document.querySelector(".responsive");
 let menuClosed = true;
 
 menuBtn.addEventListener("click", () => {
-  if(menuClosed) {
+  if (menuClosed) {
     menuBtn.classList.add("open");
     menuRespond.classList.add("responsiveBlock");
     menuClosed = false;
   } else {
     menuBtn.classList.remove("open");
-    menuClosed = true;   
+    menuClosed = true;
     menuRespond.classList.remove("responsiveBlock");
   }
-})
+});
+
+
+const sizeRadio = document.getElementById("split");
+const sizeInput = document.querySelector(".two");
+const chunk = document.querySelector(".one");
+sizeInput.disabled = true
+
+
+
+function disableInput() {
+  if (sizeRadio.checked) {
+    chunk.disabled = true;
+    sizeInput.disabled = false;
+  } else {
+    sizeInput.disabled = true;
+    chunk.disabled = false;
+  }
+}
